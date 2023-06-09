6aca517aa9a9a83d0cab59e0d463233c8c3057e5
Rajeev Krishna <lambo2572@gmail.com>
2023-06-09 23:42:21 +0530

    Started with basics of Python

---
+++ basics.py
@@ -0,0 +1,28 @@
+#Basic program in python
+print("Hello world")
+
+# This is comment in python
+""" This is 
+a multiline comment 
+in python"""
+
+# Variables in python
+a = 10
+b = 15
+print(a+b)
+
+#typeCasting in python
+x = float(4)
+y = int(3.5)
+z = str(2)
+print(x,y,z)
+print(type(x),type(y),type(z))
+
+#list or tuple
+fruits =['apple','banana','orange']
+d,e,f = fruits
+print(d,e,f)
+
+# if statement
+if 5 > 2:
+    print("Five is greater than two!")
