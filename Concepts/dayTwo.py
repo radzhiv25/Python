aecdd5e14cc13b0ee5dc1a8f817f86070c93d971
Rajeev Krishna <radzhiv@192.168.1.2>
2024-03-12 22:47:33 +0530

    concepts folder created

---
+++ Concepts/dayTwo.py
@@ -0,0 +1,15 @@
+# this is used to comment a single line
+
+'''
+this 
+is a 
+multiline comment
+'''
+
+print("Hello this is Python3 \n with a new line") # print is a function to output your code 
+
+print("Hey, it\'s python 👋🏼") 
+
+print("Hello", 9, sep="-")
+print("Hello", 9, end="👋🏼")
+print("hey")
