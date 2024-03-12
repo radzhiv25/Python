604dff51511353b49c80a631da551860dc5a8b71
Rajeev Krishna <radzhiv@192.168.1.2>
2024-03-12 22:54:02 +0530

    Calculator made

---
+++ Exercise/excercise1.py
@@ -0,0 +1,12 @@
+# Basic Calculator
+
+a = 10
+b = 6
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
+print("Modulus: ", a%b)
+print("Exponent: ", a**b)
+print("Floor Division: ", a//b)
