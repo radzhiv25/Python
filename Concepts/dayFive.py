eef927e0178e8aa5ec7f0d9b80a515ca4d048727
radzhiv <radzhivkrishna@gmail.com>
2024-03-14 23:03:32 +0530

    Learned: few methods

---
+++ Concepts/dayFive.py
@@ -0,0 +1,14 @@
+# string methods
+
+a = 'Rajeev'
+print(a.upper())
+print(a.lower())
+
+b = 'Hello this is Python'
+print(b.split()) # this makes a list
+
+c = 'Hello this is python, python is a programming language'
+print(c.replace('python', 'java'))
+
+d = 'introduction to 30day Challenge'
+print(d.capitalize())
